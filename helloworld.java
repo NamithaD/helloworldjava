8b9068e2e485feb5eb35e4b4e73d7e21abd97d16
NamithaD <106362968+NamithaD@users.noreply.github.com>
2022-06-21 09:51:15 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World changes1 done after webhook");
+    System.out.println("Hello World changes2 done after webhook");
     }
 }
